d292f5e25d5a2ba65243053be8d8e402af90d62f
mohamedhassanofficial2025-bit <mohamedhassan.official2025@gmail.com>
2026-04-21 18:58:53 +0200

    Add footer component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
 import { Toast } from './components/toast/toast';
+import { Footer } from './components/footer/footer';
 import { ThemeService } from './services/theme.service';
 
 @Component({
   selector: 'app-root',
-  imports: [Navbar, RouterOutlet, Toast],
+  imports: [Navbar, RouterOutlet, Toast, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
